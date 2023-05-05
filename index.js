9294b93aa7aba6b3555162de9cf8ad8580f24384
ElainePentium <gentil.stephanie@gmail.com>
2023-05-05 12:14:56 +0200

    fight, dodge, fumble, isAlive

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const {Fighter} = require('./src/Fighter.js');
+
+const heracles = new Fighter("💪🧔 Heracles", 20, 6);
+const lion = new Fighter("🦁 Nemean Lion", 11, 13);
+
+console.log(`${heracles.name}\t💛 ${heracles.life}`);
+console.log(`${lion.name}\t💛 ${lion.life}`);
+console.log('\n⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️\n');
+let round = 1;
+const intervalID = setInterval(() => {
+    console.log(`🕛 Round ${round} 🔔`);
+    heracles.fight(lion);
+    if (!lion.isAlive()) {
+        console.log('\n⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️\n');
+        console.log(`💀 ${lion.name} is dead`);
+        console.log(`🏆 ${heracles.name} wins! (💛${heracles.life})`);
+        if (lion.life < 0) {
+            console.log('💀💀💀 FATALITY 💀💀💀');
+        }
+        clearInterval(intervalID);
+    } else {
+        lion.fight(heracles);
+        if (!heracles.isAlive()) {
+            console.log('\n⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️➖⚔️\n');
+            console.log(`💀 ${heracles.name} is dead`)
+            console.log(`🏆 ${lion.name} wins! (💛${lion.life})`)
+            if (heracles.life < 0) {
+                console.log('💀💀💀 FATALITY 💀💀💀')
+            }
+            clearInterval(intervalID);
+        } else {
+            console.log('');
+        }
+    }
+    round++;
+
+
+}, 800);
+
+// In the index.js file, create a loop for performing a fight to the death! As long as one of the two enemies has a health > 0, the combat therefore continues.
+
+// In each "round" 🕛, Heracles attacks the lion, then the lion attacks Heracles. You have to display the round number, who attacks who, and the remaining life points.
+
+// When one of the two is defeated, you must then display the winner 🏆 and the loser 💀. The screenshot below gives you a summary of what to expect.
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,52 @@
 /* Fighter class definition */
+MAX_LIFE = 100;
+
+class Fighter {
+    constructor (name, strength, dexterity) {
+        // name: name of the fighter.
+        this.name = name;
+        // strength: will be used to calculate the damage points during an attack.
+        this.strength = strength;
+        // dexterity: will be used to calculate the defense points that will limit the damage received.
+        this.dexterity = dexterity;
+        // life: initialized to MAX_LIFE, so they all start with 100 life points.
+        this.life = MAX_LIFE;
+    }
+
+    fight(defender) {
+        const attack = (this.strength * Math.random()).toFixed(0);
+
+        // attack = (0 | 1) --> FUMBLE
+        if (attack - defender.dexterity <= (1 - defender.dexterity)) {
+            this.life -= 1
+            console.log(`${this.name} missed, stumbled and lost 1 life point 😵‍💫💔 [FUMBLE] 💛${this.life}`)
+        } 
+
+        // attack <= half the defender's dexterity --> def DODGE
+        else if (attack - defender.dexterity <= -(defender.dexterity/2)) {
+            console.log(`${this.name} 🗡️ ${defender.name} ➖ ${defender.name} dodged the attack 💨 💛${defender.life}`)
+        }
+
+        // attack <= defender's dexterity --> def DEFEND (thick skin | shield)
+        else if (attack - defender.dexterity <= 0) {
+            console.log(`${this.name} 🗡️ ${defender.name} ➖ ${defender.name} defended 🛡️ 💛${defender.life}`)
+        }
+
+        // attack > defender's dexterity --> DAMAGE
+        else {
+            defender.life -= attack;
+            console.log(`${this.name} 🗡️ ${defender.name} ➖ Ouch! ${defender.name} 💛${defender.life > 0 ? defender.life : 0}`);
+        }
+    }
+
+    isAlive() {
+        if (this.life > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+module.exports = {Fighter}
